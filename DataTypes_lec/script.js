1fb4c8e404514a87877c6c70781d25226571a394
rohitfating <rohitfating@gmail.com>
2026-07-23 10:32:06 +0530

    DataTypes

---
+++ DataTypes_lec/script.js
@@ -0,0 +1,17 @@
+// Primitive datatypes -
+
+// number => 2 to the power 53
+// bigint
+// string => " "
+//boolean => true/false
+//null => standalone value
+//undefined => Not defined any value
+//symbol => unique
+
+//Non-Primitive datatypes -
+
+//object
+
+console.log(typeof"Alex") ;  // string
+console.log(typeof null) ;  // object
+console.log(typeof undefined) ;  // undefined
